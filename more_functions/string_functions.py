e617f759474f237472148d25faf9bda50987453f
Syed Rehman <syedr37@gmail.com>
2020-06-28 19:42:50 -0500

    updated function. passes test

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,4 +1,13 @@
 def multiply_string(message, n):
-    """Prints a message 'n' amount of times"""
+    """Returns a string 'n' amount of times
 
-    return message * n
+    Simply multiplies a string n amount of times and returns it.
+
+    :param string message: Message/String to multiply
+    :param int n: Number of times to multiply message/string
+    :return: If message is 'message' and n is '3', return will be messagemessagemessage
+    :rtype: stringe
+
+    """
+
+    return str(message) * n
